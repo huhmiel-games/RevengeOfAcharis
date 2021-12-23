8eacf3a46c1881e356b6bca730419335526a4ec4
philippe Pereira <phil.pereira@protonmail.com>
2021-12-23 18:44:30 +0100

    remove unused files

--- src/utils/counDeadEnemies.js
+++
@@ -1,6 +0,0 @@
-export default async () => {
-  let en = localStorage.getItem('e');
-  en = JSON.parse(en);
-  en += 1;
-  localStorage.setItem('e', en);
-};
--- src/utils/countTime.js
+++
@@ -1,8 +0,0 @@
-export default (e) => {
-  const secondTimestamp = new Date().getTime();
-  const result = secondTimestamp - e;
-  let elapsedTime = localStorage.getItem('time');
-  elapsedTime = JSON.parse(elapsedTime);
-  localStorage.setItem('time', elapsedTime + result);
-  return new Date().getTime();
-};
