09e72b37299433ad5c5c1fb00018e2f19d34a8ef
philippe Pereira <phil.pereira@protonmail.com>
2022-01-04 06:00:07 +0100

    map improvment

--- buildAnimationTileset.js
+++ buildAnimationTileset.js
@@ -726,7 +726,7 @@ function getFile (filename)
 }
 
 //parsing json
-for (let i = 1; i < 35; i++)
+for (let i = 1; i < 36; i++)
 {
     var jsonString = [getFile(`./src/maps/map${ i }.json`)];
     var jsonObj = JSON.parse(jsonString);
